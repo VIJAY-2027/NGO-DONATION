8437793de759a935f4e00b1d8e9bf4bd909615b7
VIJAY S <vvvijay17102005@gmail.com>
2026-05-26 13:56:13 +0530

    Create DuplicateDonorException.java

---
+++ DuplicateDonorException.java
@@ -0,0 +1,9 @@
+package com.wipro.ndms.util;
+
+public class DuplicateDonorException  extends Exception {
+
+    public DuplicateDonorException(String message) {
+        super(message);
+    }
+
+}
